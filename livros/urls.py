51f418c9675d5765e1c989cdcab3a8c5df69f54a
Gabriela de Melo Freitas <45526185800@>
2025-04-07 10:39:28 -0300

    task 1'

--- livros/urls.py
+++ livros/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('livros/', views.lista_livros, name='lista_livros'),
+    path('api/livros', views.livros_api),
 ]
--- livros/views.py
+++ livros/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from .models import Livro
+from .serializers import LivroSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 def lista_livros(request):
     livros = Livro.objects.all()
     return render(request, 'livros.html', {'livros': livros})
+
+@api_view (['GET', 'POST'])
+
+def livros_api(request):
+    if request.method == 'GET':
+        livros = Livro.objects.all()
+        serializer = LivroSerializer(livros, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = LivroSerializer(data=request.data, many=isinstance(request.data, list))
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
