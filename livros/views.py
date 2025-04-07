c07d3ba958126f91fee3476cdffc9f5467acc466
Gabriela de Melo Freitas <45526185800@>
2025-04-07 10:03:59 -0300

    task 1'

--- livros/views.py
+++ livros/views.py
@@ -4,4 +4,4 @@ from .models import Livro
 # Create your views here.
 def lista_livros(request):
     livros = Livro.objects.all()
-    return render(request, 'lista_livros.html', {'livros': livros})
+    return render(request, 'livros.html', {'livros': livros})
