521e3f71e907cba88cfdef7a1ccac7729ce45aac
Gabriela de Melo Freitas <45526185800@>
2025-04-07 10:07:52 -0300

    task 1

--- livros/models.py
+++ livros/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Livro(models.Model):
     titulo = models.CharField(max_length=50)
     paginas = models.IntegerField()
-    autor = models.CharField(max_length=50)
+    autor = models.CharField(max_length=30)
 
     def __str__(self):
             return self.titulo
